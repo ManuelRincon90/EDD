7cfdaf9319ade8a10debaad9d2671452b479243b
dasuniel <115125034+dasuniel@users.noreply.github.com>
2024-07-24 12:31:34 -0500

    Add files via upload

---
+++ Ejercicios/reto00.cpp
@@ -0,0 +1,16 @@
+/**************************************************************
+Autor: Daniel Rosas
+Fecha: 17/07/2024
+Materia: Estructuras de Datos
+Temas: Presentacion de C++, Plantillas Principales
+**************************************************************/
+
+#include <iostream>
+
+int main(int argc, char* argv[]){
+
+	std::cout << "Hola Mundo" << std::endl;
+	
+	return 0;
+	
+	}
