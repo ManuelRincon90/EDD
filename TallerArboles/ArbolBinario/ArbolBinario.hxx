96c206068e6c91cf846835d01f41e16d55832c61
dasuniel <115125034+dasuniel@users.noreply.github.com>
2024-10-02 11:51:31 -0500

    Update ArbolBinario.hxx

--- TallerArboles/ArbolBinario/ArbolBinario.hxx
+++ TallerArboles/ArbolBinario/ArbolBinario.hxx
@@ -1,3 +1,10 @@
+/*****************************************************************************************************************************
+Fecha: 1 octubre 2024
+Autor: Daniel Rosas
+Tema: Taller Arboles
+Estructuras de datos
+******************************************************************************************************************************/
+
 #include "ArbolBinario.h"
 #include "NodoBinario.h"
 
@@ -74,7 +81,8 @@ bool ArbolBinario<T>::eliminar(T& val)
         //Nodo* nuevo = borrar->hijoIzq->extremo_der();
 		NodoBinario<T>* nuevo = borrar->hijoIzq->extremo_der();
         borrar->fijarDato(nuevo->obtenerDato());
-		delete(nuevo);    }
+		delete(nuevo);
+    }
     else if(borrar->hijoDer != NULL)
     {
 		
