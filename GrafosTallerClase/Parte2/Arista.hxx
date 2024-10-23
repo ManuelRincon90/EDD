63995b5427e73967b74c1bf1b57c98e09032d09f
dasuniel <115125034+dasuniel@users.noreply.github.com>
2024-10-23 12:15:43 -0500

    Update Arista.hxx

--- GrafosTallerClase/Parte2/Arista.hxx
+++ GrafosTallerClase/Parte2/Arista.hxx
@@ -1,6 +1,12 @@
+/*****************************************************************************************************************************
+Fecha: 23 octubre 2024
+Autor: Daniel Rosas
+Tema: Taller Grafos Clase 2
+Estructuras de datos
+******************************************************************************************************************************/
+
 #include "Arista.h"
 
 template <typename T, typename W>
 Arista<T, W>::Arista(std::shared_ptr<Vertice<T, W>> dest, W peso)
     : destino(dest), peso(peso) {}
-
